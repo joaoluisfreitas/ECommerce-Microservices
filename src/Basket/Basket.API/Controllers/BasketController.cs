f7e7cc97083f25ecb7833965da0c84a4a556458b
Joao Freitas <joaoluisfreitas@gmail.com>
2021-02-02 14:05:24 -0300

    Implementation of the Basket Service

--- src/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Basket/Basket.API/Controllers/BasketController.cs
@@ -1,12 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Basket.API.Entities;
+using Basket.API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Basket.API.Controllers
 {
+    [Route("api/v1/[controller]")]
+    [ApiController]
     public class BasketController : ControllerBase
     {
+        private readonly IBasketRepository _repository;
+        private readonly ILogger<BasketController> _logger;
+
+        public BasketController(IBasketRepository repository, ILogger<BasketController> logger)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _logger = logger ?? throw new NotImplementedException(nameof(logger));
+        }
+
+        [HttpGet("{userName:length(50)}", Name ="GetBasket")]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(IEnumerable<BasketCart>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<BasketCart>> GetBasket(string userName)
+        {
+            var basket = await _repository.GetBasket(userName);
+            if (basket == null)
+            {
+                _logger.LogError($"Basket for user {userName} not found");
+                return NotFound();
+            }
+            else return Ok(basket);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(IEnumerable<BasketCart>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<BasketCart>> UpdateBasket([FromBody] BasketCart basketCart)
+        {
+            return Ok(await _repository.UpdateBasket(basketCart));
+        }
+
+        [HttpDelete("{userName:length(50)}", Name ="DeleteBasket")]
+        [ProducesResponseType(typeof(IEnumerable<BasketCart>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<BasketCart>> UpdateBasket(string userName)
+        {
+            return Ok(await _repository.DeleteBasket(userName));
+        }
+
     }
 }
--- src/Basket/Basket.API/Startup.cs
+++ src/Basket/Basket.API/Startup.cs
@@ -1,14 +1,14 @@
+using Basket.API.Data;
+using Basket.API.Data.Interfaces;
+using Basket.API.Repositories;
+using Basket.API.Repositories.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using StackExchange.Redis;
+
 
 namespace Basket.API
 {
@@ -24,6 +24,18 @@ namespace Basket.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<ConnectionMultiplexer>(sp =>
+                {
+                    var configuration = ConfigurationOptions.Parse(Configuration.GetConnectionString("Redis"), true);
+                    return ConnectionMultiplexer.Connect(configuration);
+                });
+
+            services.AddTransient<IBasketContext, BasketContext>();
+            services.AddTransient<IBasketRepository, BasketRepository>();
+
+            services.AddSwaggerGen(c =>
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Basket API", Version = "v1" })
+            );
             services.AddControllers();
         }
 
@@ -43,6 +55,8 @@ namespace Basket.API
             {
                 endpoints.MapControllers();
             });
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Basket API v1"));
         }
     }
 }
